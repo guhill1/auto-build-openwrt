89e0d612cc35d9c19a2428c8d910e7433cf50340
guhill1 <124128440+guhill1@users.noreply.github.com>
2024-09-13 07:09:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,6 +14,6 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # Modify compile version
-sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.1/g' target/linux/x86/Makefile
 build_date=$(date +'%m-%d-%Y')
 sed -i "s/<%=pcdata(ver.distname)%> <%=pcdata(ver.distversion)%> /<%=pcdata(ver.distname)%> <%=pcdata(ver.distversion)%> guhill $build_date /g" package/lean/autocore/files/x86/index.htm
